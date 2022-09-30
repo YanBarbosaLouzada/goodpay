9be67ce466ba5aeb0ba1588cec470f73b79e8972
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2022-09-30 17:28:16 -0300

    Alteração dia 30/09/2022

---
+++ js/moduloDados.js
@@ -0,0 +1,36 @@
+/**
+ * Dentro deste array existem usuarios com esta estrutura 
+ * id 
+ * nomeUsuario
+ * senha 
+ * email 
+ *
+ */
+
+
+const usuarios=[
+    {id:1,
+    nomeUsuario: 'yan barbosa',
+    senha:'12345',
+    email: 'yan.2@gmail.com'
+    },
+    {id:2,
+        nomeUsuario: 'felipe barbosa',
+        senha:'12345',
+        email: 'felipe.2@gmail.com'
+        },
+    {id:3,
+     nomeUsuario: 'pedro barbosa',
+     senha:'12345',
+     email: 'pedro.2@gmail.com'
+    }   
+
+
+]
+
+export default usuarios;
+
+// Pode ser usados em outros arquivos
+
+
+
