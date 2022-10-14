ff0b8ba2fec6fa14a09a003246ef5f388a406938
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2022-10-14 11:55:20 -0300

    ProjetoGoodpay

--- js/moduloDados.js
+++ js/moduloDados.js
@@ -1,39 +1,31 @@
-/**
- * Dentro deste array existem usuarios com esta estrutura 
- * id 
- * nomeUsuario
- * senha 
- * email 
- *
- */
-
-
-const usuarios=[
-    {id:1,
-    nomeUsuario:'yan barbosa',
-    senha:'12345',
-    email: 'yan.2@gmail.com',
-    
-    },
-    {id:2,
-        nomeUsuario: 'felipe barbosa',
-        senha:'12345',
-        email: 'felipe.2@gmail.com',
-        
-        },
-    {id:3,
-     nomeUsuario: 'pedro barbosa',
-     senha:'12345',
-     email: 'pedro.2@gmail.com',
-    
-    }   
-
-
-]
+const usuarios = [
+  {
+    id: 1,
+    nomeUsuario: "batata",
+    senha: "12345",
+    email: "jose.almir@soulcodeacademy.org",
+  },
+  {
+    id: 2,
+    nomeUsuario: "renato3x",
+    senha: "12345",
+    email: "renato.pereira@gmail.com",
+  },
+  {
+    id: 3,
+    nomeUsuario: "bruno-dias",
+    senha: "12345",
+    email: "brunos.dias@gmail.com",
+  },
+  {
+    id: 4,
+    nomeUsuario: "pedro-viana",
+    senha: "12345",
+    email: "pedro.viana@gmail.com",
+  },
+];
 
 export default usuarios;
-
-// Pode ser usados em outros arquivos
-
-
-
+/**
+ * Disponibiliza o array para ser utilizado fora do arquivo moduloDados.js
+ */
